2a845af0116f526092d1e4da7d9af592571c1085
Oskar <oskar.calvo@aeg.eus>
2025-10-28 13:25:12 +0100

    [UPDATE] execution page

--- src/pages/execution.tsx
+++ src/pages/execution.tsx
@@ -2,8 +2,6 @@ import { useRouter } from 'next/router';
 import Layout from '@/components/Layout'
 import React from 'react'
 
-
-
 const execution = () => {
   const router = useRouter();
 
@@ -17,11 +15,11 @@ const execution = () => {
 
   return (
     <Layout>
-      <div className='flex justify-around'>
-        <div className="relative border border-sepia bg-black/30 rounded-md p-2 bg-cover bg-center cursor-pointer" onClick={handleBonificationsRouter}>
+      <div className='flex justify-around p-5'>
+        <div className="w-1/3 border border-sepia bg-black/30 rounded-md p-2 bg-cover bg-center cursor-pointer hover:border-white" onClick={handleBonificationsRouter}>
           <p className="text-3xl text-medievalSepia text-center  mb-6">Bonifications</p>
           </div>
-        <div className="relative border border-sepia bg-black/30 rounded-md p-2 bg-cover bg-center cursor-pointer" onClick={handleFeedbackRouter}>
+        <div className="w-1/3 border border-sepia bg-black/30 rounded-md p-2 bg-cover bg-center cursor-pointer hover:border-white" onClick={handleFeedbackRouter}>
           <p className="text-3xl text-medievalSepia text-center  mb-6">Feedback</p>
         </div>
       </div>
